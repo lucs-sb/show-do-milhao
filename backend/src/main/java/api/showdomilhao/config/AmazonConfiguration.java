b107182683560ec4f40e30cc735c0d719f07969d
Lucas Soares <soares0lucas8@gmail.com>
2023-11-22 19:00:43 -0300

    refactor: alterando conexão com a aws

--- backend/src/main/java/api/showdomilhao/config/AmazonConfiguration.java
+++ backend/src/main/java/api/showdomilhao/config/AmazonConfiguration.java
@@ -2,6 +2,7 @@ package api.showdomilhao.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,21 +11,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfiguration {
-    @Value("${amazon.s3.acess.key}")
-    private final String ACCESS_KEY="[Minha chave de acesso]";
-    @Value("${amazon.s3.secret.key}")
-    private final String SECRET_KEY="[Minha chave de segredo]";
     @Value("${amazon.s3.region}")
-    private final String REGION="us-east-1";
-
-    @Bean
-    public BasicAWSCredentials basicAWSCredentials(){
-        return new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
-    }
+    private String REGION;
 
     @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard().withRegion(REGION)
-                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials())).build();
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .build();
     }
 }
