29e04ceccdeac2bbb8eb1d88f8679920bf6a88de
Lucas Soares <soares0lucas8@gmail.com>
2023-12-10 10:20:59 -0300

    refactor: ajustando query na AnswerRepository

--- backend/src/main/java/api/showdomilhao/repository/AnswerRepository.java
+++ backend/src/main/java/api/showdomilhao/repository/AnswerRepository.java
@@ -9,10 +9,10 @@ import java.util.Set;
 
 @Repository
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
-    @Query("SELECT a.* FROM tb_answer AS a " +
+    @Query(value = "SELECT a.* FROM tb_answer AS a " +
             "INNER JOIN tb_question_answer AS qa ON qa.answer_id = a.answer_id " +
-            "INNER JOIN tb_match_answer AS ma ON ma.question_id = qa.question_id AND ma.answer_id = qa.answer_id " +
+            "INNER JOIN tb_match_answer AS ma ON ma.answer_id = qa.answer_id " +
             "WHERE qa.question_id = :questionId AND ma.match_id = :matchId AND ma.deleted = 0 " +
-            "ORDER BY RAND()")
+            "ORDER BY RAND()", nativeQuery = true)
     Set<Answer> findAnswersByMatchIdAndQuestionId(Long matchId, Long questionId);
 }
