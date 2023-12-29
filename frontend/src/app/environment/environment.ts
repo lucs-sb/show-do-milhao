ff6fd7b2dc642cf7a1006fbc21ac198492439796
Lucas Soares <soares0lucas8@gmail.com>
2023-12-29 18:12:31 -0300

    feat: url backend

--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    API_URL_DEFAULT: 'http://localhost:8080/api/',
+    production: true,
+    API_URL_DEFAULT: 'ec2-44-200-255-205.compute-1.amazonaws.com/api/',
     timePerQuestion: 30
 }
