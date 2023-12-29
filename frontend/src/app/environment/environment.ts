946e837c8d5d5cf3c60c879c7a376395deb4cdff
Lucas Soares <soares0lucas8@gmail.com>
2023-12-29 19:07:10 -0300

    ajustes

--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL_DEFAULT: 'ec2-44-200-255-205.compute-1.amazonaws.com/api/',
+    API_URL_DEFAULT: 'http://ec2-44-200-255-205.compute-1.amazonaws.com/api/',
     timePerQuestion: 30
 }
